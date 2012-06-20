977c10f28529068a01200b386b8fd7d9de49628a
Hiram Chirino <hiram@hiramchirino.com>
2012-06-20 16:35:20 -0400

    Adding/Fixing License Headers.

--- fusesource-amqp/src/test/java/org/fusesource/amqp/AMQPTest.java
+++ fusesource-amqp/src/test/java/org/fusesource/amqp/AMQPTest.java
@@ -1,3 +1,20 @@
+/**
+ * Copyright (C) 2012 FuseSource Corp. All rights reserved.
+ * http://fusesource.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.fusesource.amqp;
 
 import org.junit.Test;
@@ -20,6 +37,7 @@ public class AMQPTest {
 
         final AMQPClientOptions clientOptions = new AMQPClientOptions();
         clientOptions.setHost(new URI("tcp://localhost:61613"));
+
         AMQP.open(clientOptions, new Callback<AMQPConnection>() {
             public void onSuccess(final AMQPConnection connection) {
                 AMQPSessionOptions sessionOptions = new AMQPSessionOptions(100, 1000);
